62a27934bae6cb1545c8644e51e28293da62b2df
Jonathan Peppers <jonathan.peppers@microsoft.com>
2019-03-04 08:21:11 -0600

    Simple AOT repro

--- AotRepro/AotRepro/MainPage.xaml.cs
+++ AotRepro/AotRepro/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ namespace AotRepro
 		public MainPage ()
 		{
 			InitializeComponent ();
+
+			var foo = DataRowComparer.Default;
 		}
 	}
 }
